92111d01658aeb16acc5f3ec8465b9fe379fbb3c
Julie P <paupertjulie@gmail.com>
2020-09-01 16:10:05 +0200

    chat footer styling

--- src/Components/ChatFooter.js
+++ src/Components/ChatFooter.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 function ChatFooter() {
     return (
         <div className="chat__footer">
-            
+            <InsertEmoticonIcon />
+            <form action="">
+                <input type="text" placeholder="Taper un message"/>
+                <button>Envoyer</button>
+            </form>
+            <MicIcon />
         </div>
     )
 };
