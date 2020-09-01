e8b57a78b91db14c1568cfeb81dc6f0073d6fe6c
Julie P <paupertjulie@gmail.com>
2020-09-01 16:17:47 +0200

    styling done

--- src/Components/ChatFooter.js
+++ src/Components/ChatFooter.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 
 function ChatFooter() {
+
+    const [ input, setInput ] = useState('');
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log('You typed >>>', input)
+    };
+
     return (
         <div className="chat__footer">
             <InsertEmoticonIcon />
             <form action="">
-                <input type="text" placeholder="Taper un message"/>
-                <button>Envoyer</button>
+                <input 
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                type="text" 
+                placeholder="Taper un message"/>
+                <button 
+                onClick={sendMessage}
+                type="submit">
+                Envoyer
+                </button>
             </form>
             <MicIcon />
         </div>
