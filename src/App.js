8c16e2cac2792d7e480a75ea16a87cc86481db3d
Julie P <paupertjulie@gmail.com>
2020-09-01 15:45:27 +0200

    chat body styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     //BEM naming convention
     <div className="app">
-
+      
       <div className="app__body">
         {/* Sidebar */}
         <Sidebar />
--- src/Components/ChatBody.js
+++ src/Components/ChatBody.js
@@ -3,7 +3,11 @@ import React from 'react';
 function ChatBody() {
     return (
         <div className="chat__body">
-            
+            <p className={`chat__message ${true && 'chat__receiver'}`}>
+                <span className="chat__name">Lily</span>
+            Coucou !  
+            <span className="chat__timestamp">00:09am</span> 
+            </p>
         </div>
     )
 };
