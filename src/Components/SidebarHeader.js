54b18c5696f5d343a587a792d5b4f2d968a55ddc
Julie P <paupertjulie@gmail.com>
2020-09-02 14:56:52 +0200

    get user picture with google auth

--- src/Components/SidebarHeader.js
+++ src/Components/SidebarHeader.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useStateValue } from '../StateProvider';
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVert from '@material-ui/icons/MoreVert';
 
 function SidebarHeader() {
+
+    const [ { user }, dispatch ] = useStateValue();
+
     return (
         <div className="sidebar__header">
-            <Avatar />
+            <Avatar src={user?.photoURL}/>
             <div className="sidebar__headerRight">
                 <IconButton>
                     <DonutLargeIcon />
